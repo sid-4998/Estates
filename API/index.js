0336256ed20c6db6d81998f7b9a24cd858811e87
Your Name <siddhantjha34@gmail.com>
2024-03-28 18:38:08 +0530

    Created a Test API Route

--- API/index.js
+++ API/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 process.loadEnvFile(".env");
 mongoose
 .connect(process.env.MONGO)
@@ -10,6 +11,8 @@ mongoose
     console.log(err);
 });
 const app = express();
+
+app.use('/API/user', userRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
