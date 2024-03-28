f4010216d7ba343524ea76cdf32e862e70244c45
Your Name <siddhantjha34@gmail.com>
2024-03-28 19:44:19 +0530

    Created SignUp API Route but must be revisited for better error handling

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 process.loadEnvFile(".env");
 mongoose
 .connect(process.env.MONGO)
@@ -12,7 +13,9 @@ mongoose
 });
 const app = express();
 
+app.use(express.json());
 app.use('/API/user', userRouter);
+app.use('/API/auth', authRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
